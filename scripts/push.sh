b4ba00f8f3e67a5ea335d079d74e3e12e0eca12e
PatrickCoin <vidtion@gmail.com>
2017-05-10 18:40:57 +0800

    modify push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-printf '\n\n>>>>>>>>>>>>>>>>>> cd /Users/coin/Documents/WorkingCopys/TouTiao/Toutiao-React/build\n\n'
-cd /Users/coin/Documents/WorkingCopys/TouTiao/Toutiao-React/build
+printf '\n\n>>>>>>>>>>>>>>>>>> cd /Users/coin/Documents/GitRepository/babymarket-web-src/build\n\n'
+cd /Users/coin/Documents/GitRepository/babymarket-web-src/build
 printf '\n\n>>>>>>>>>>>>>>>>>> ls -la\n\n'
 ls -la
 printf '\n\n>>>>>>>>>>>>>>>>>> cp -rvf ./* /Users/coin/Documents/GitRepository/babymarket-web-build\n\n'
 cp -rvf ./* /Users/coin/Documents/GitRepository/babymarket-web-build
-printf '\n\n>>>>>>>>>>>>>>>>>> cd /Users/coin/Documents/GitRepository/react-bm\n\n'
-cd /Users/coin/Documents/GitRepository/react-bm
+printf '\n\n>>>>>>>>>>>>>>>>>> cd /Users/coin/Documents/GitRepository/babymarket-web-build\n\n'
+cd /Users/coin/Documents/GitRepository/babymarket-web-build
 printf '\n\n>>>>>>>>>>>>>>>>>> git status\n\n'
 git status
 printf '\n\n>>>>>>>>>>>>>>>>>> git add .\n\n'
